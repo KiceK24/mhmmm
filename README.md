3c8f3f028b530a26488c7416edc1ec9a1ae5f1af
KiceK24 <83403056+KiceK24@users.noreply.github.com>
2021-04-29 14:42:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mhmmm
+# gugiz
+ 
---
+++ config/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    nameofbot: "VulcanaRP", // Podaje nazwe bota
+    colorofbot: "#67087b", // Podaj kolor jaki chcesz uzywac w bocie
+    prefix: "&", // Prefix na jaki bedzie reagowal bot
+    ip: "91.223.3.169:6962", // Ip servera z którego bedzie pobieralo ilosc graczy do statusu np: 123.123.213.213:12312
+    token: "", // token bota
+    refreshInteval: "20000", // nie ruszaj
+    weryfikacjarola: "", // ustaw role ktora daje po uzyciu verify
+}
+
